ca170e6ea9902551f0dcbd4181096ac13cbdcc2f
Ocaanaaa <166707320+Ocaanaaa@users.noreply.github.com>
2024-07-16 08:22:18 +0000

    [Final Counter]

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,15 @@
 import React from "react";
+import Digit from "./digit";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = ({ digit }) => {
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5">{digit}</h1>
+      <Button as="input" type="reset" value="Reset" />
+    </div>
+  );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,14 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import Contador from "./component/contador.jsx";
 
+let number = 0;
+
+setInterval(function () {
+    number++;
+    ReactDOM.createRoot(document.getElementById('app')).render(<Contador number={number} />)
+}, 1000);
 //render your react application
 ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
 
